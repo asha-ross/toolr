1ac11ca12607a0b11a6e079b1b55d5965a19bd62
Andrew Keatinge <andrewgkeatinge@gmail.com>
2024-09-05 00:40:42 +1200

    users are being added to the table correctly

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -9,15 +9,13 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 import { addUser } from '../apis/tools'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { Users } from '../../models/tools'
+
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState('')
 
   const { user, getAccessTokenSilently } = useAuth0()
 
-  const queryClient = useQueryClient()
 
   const [token, setToken] = useState('')
 
@@ -35,28 +33,28 @@ export default function Home() {
     }
   }, [user, getAccessTokenSilently, token]);
 
-  const addUserMutation = useMutation({
-    mutationFn: (user: Users) => addUser(user, token),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['user'] }),
-  })
+  // const addUserMutation = useMutation({
+  //   mutationFn: (user: Users) => addUser(user, token),
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ['user'] }),
+  // })
 
-  // const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-  //  e.preventDefault()
-  //  //TODO: Need to input some search logic here
-  // }
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    addUserMutation.mutate({
-      auth_id: String(user?.sub),
-      username: String(user?.nickname),
-      created_at: new Date(),
-    })
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+   e.preventDefault()
+   //TODO: Need to input some search logic here
   }
+
+  // const handleSubmit = (e: React.FormEvent) => {
+  //   e.preventDefault()
+  //   addUserMutation.mutate({
+  //     auth_id: String(user?.sub),
+  //     username: String(user?.nickname),
+  //     created_at: new Date(),
+  //   })
+  // }
 
   return (
     <div className="home">
-      <form onSubmit={(event) => handleSubmit(event)} className="search-form">
+      <form onSubmit={(event) => handleSearch(event)} className="search-form">
         <input
           type="text"
           value={searchTerm}
