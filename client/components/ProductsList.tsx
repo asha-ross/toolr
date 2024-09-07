ca2edc5435ad5cd2ad102eb310e93b41e1f86442
Andrew Keatinge <andrewgkeatinge@gmail.com>
2024-09-07 17:16:13 +1200

    category filter added on product list page

--- client/components/ProductsList.tsx
+++ client/components/ProductsList.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { Tools } from '../../models/tools'
 import { getTools } from '../apis/tools'
+import { useState } from 'react'
 
 export default function GetAllProducts() {
   const {
@@ -12,6 +13,22 @@ export default function GetAllProducts() {
     queryKey: ['tools'],
     queryFn: getTools,
   })
+  
+  const [filteredTools, setFilteredTools] = useState<Tools[] | undefined>(tools)
+
+  const filterTools = (category: string) => {
+    if (category === "All") {
+      setFilteredTools(tools)
+    } else {
+      const filtered = tools?.filter(tools => tools.category.includes(category))
+      console.log(filtered)
+      setFilteredTools(filtered)
+    }
+  };
+
+  const displayAllTools = () => {
+    setFilteredTools(tools)
+  }
 
 
   if (isLoading) return <div>Loading Tools...</div>
@@ -19,11 +36,26 @@ export default function GetAllProducts() {
 
   return(
     <>
+
     <div className='products-container'>
+    <div className="dropdown">
+          <button className="dropbtn">Filter by Catergory</button>
+          <div className="dropdown-content">
+            <button className="categoryButton" onClick={displayAllTools}>All</button>
+            <button className="categoryButton" onClick={() => filterTools('Small Power Tools')}>Small Power Tools</button>
+            <button className="categoryButton" onClick={() => filterTools('Small Power Tools')}>Yard & Garden</button>
+            <button className="categoryButton" onClick={() => filterTools('Small Power Tools')}>Outdoor Tools</button>
+            <button className="categoryButton" onClick={() => filterTools('Carpentry Tools')}>Carpentry Tools</button>
+            <button className="categoryButton" onClick={() => filterTools('Carpentry Tools')}>Cleaning Tools</button>
+            <button className="categoryButton" onClick={() => filterTools('Carpentry Tools')}>Cleaning Tools</button>
+            <button className="categoryButton" onClick={() => filterTools('Multi-Tools')}>Multi-Tools</button>
+            <button className="categoryButton" onClick={() => filterTools('Heavy Power Tools')}>Heavy Power Tools</button>
+          </div>
+        </div>
     <div className='product-list-container'>
-      {tools && Array.isArray(tools) && tools.length > 0 && (
+      {filteredTools && Array.isArray(filteredTools) && filteredTools.length > 0 && (
         <ul className="tools-list">
-          {tools.map((tool: Tools) => (
+          {filteredTools.map((tool: Tools) => (
             <li key={tool.id} className='product-listing'>
               <img src={tool.image} alt={tool.tool_name} className='tool-container'/>
               <Link to={`/tools/${tool.id}`}>
